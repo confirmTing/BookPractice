e24e8f3da64527093b84a32967c7d787c540cf3a
confirmTing <550937586@qq.com>
2017-05-23 17:36:10 +0800

    添加示例代码

--- 你不知道的javascript-中/1-附录A/实现中的限制.js
+++ 你不知道的javascript-中/1-附录A/实现中的限制.js
@@ -16,11 +16,35 @@ function safeAdd(nums, cb){
     }
 }
 
-let arr = [];
-// 如果数组很大在浏览器就会报错 "RangeError:Maximum call stack size exceeded"
-for(let i = 0;i<1000000;i++){
-    arr.push(i)
+function safeAdd2(nums, cb){
+    let promises = [];
+    while(nums.length > 0){
+        let beforeNums = nums.splice(0,10000);
+        let promise = new Promise((resolve, reject) => {
+            setTimeout(function() {
+                resolve(cb(...beforeNums))
+            }, 0);
+        })
+        promises.push(promise);
+    }
+    return promises
 }
 
-let result = safeAdd(arr, addAll);
+let arr1 = [], arr2 = [];
+// 如果数组很大在浏览器就会报错 "RangeError:Maximum call stack size exceeded"
+for(let i = 0;i<1000000;i++){
+    arr1.push(i);
+    arr2.push(i)
+}
+let beforeTime = Date.now();
+let result = safeAdd(arr1, addAll);
+let endTime = Date.now();
+console.log(endTime - beforeTime)
 console.log(result)
+beforeTime = Date.now();
+Promise.all(safeAdd2(arr2, addAll)).then(data => {
+    let result = addAll(...data);
+    endTime = Date.now();
+    console.log(result)
+    console.log(endTime - beforeTime)
+})
